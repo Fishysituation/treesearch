fc0f5ed4fd3adcfa3fa7a85ffb1a03a8e723af46
Vincent Wang <vwang00@gmail.com>
2020-08-11 14:42:46 +0100

    Adds page layout

---
+++ popup.js
@@ -0,0 +1,9 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+};
