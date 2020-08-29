b012ff8c0a08924cf19fdbb4d28a1d9c4af724b3
Vincent Wang <vwang00@gmail.com>
2020-08-29 18:06:46 +0100

    Adds basic tree structure and creation

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
-let changeColor = document.getElementById('changeColor');
+$(document).ready(function(){
+  //add another "root node" to current "tree"
+  $('#addRoot').click(function(){
+    $('#root').append("<div class='nodeDiv'><li class='node'><p class='nodeText'>newNode</p></li></div>")
+  });
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+  $('body').on("click", ".nodeText", function(){
+    $(this).parent().append("<ul class='nodeWrapper'><li class='node'><p class='nodeText'>newNode</p></li></ul>")
+  });
 });
-
-changeColor.onclick = function(element) {
-};
